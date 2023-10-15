11d37e6dcbdf4c82f737b8c4c002fd057138a868
Pranaymate123 <pranaymate0706@gmail.com>
2023-10-15 10:49:47 +0530

    first commit after contact page

---
+++ src/main/java/maven/dao/RegisterDao.java
@@ -0,0 +1,32 @@
+package maven.dao;
+
+import java.util.List;
+
+import maven.model.ContactModel;
+import maven.model.Login;
+import maven.model.Mentor;
+import maven.model.Register;
+
+public interface RegisterDao {
+
+	public int addRecord(Register reg);
+
+	public int addMentor(Mentor ment);
+
+	public List<Mentor> validateMentor(Login log);
+
+	public List<Register> validateMentee(Login log);
+
+	public int updateRecord(Register reg);
+
+	public int updateMentorRecord(Mentor ment);
+
+	public List<Register> displayAll();
+
+	public List<Register> displayAllStud(String name);
+	
+	public int addIssue(ContactModel cm);
+	
+	public List<ContactModel> displayIssues(String name);
+
+}
